f885061d04a39662699c18f942683c586ee100e3
Jose Vargas <jvargas@basecampcodingacademy.org>
2017-08-16 11:18:32 -0500

    Learning How To Test

--- test_core.py
+++ test_core.py
@@ -1,15 +1,16 @@
-import core
+# import core
 
 # def test_new_gladiator():
 
-#     assert new_gladiator(health, rage, damage_low, damage_high) == {
+#     assert core.new_gladiator(0, 52, 20, 30) == {
 #         'Health': 0,
 #         'Damage_high': 30,
 #         'Rage': 52,
 #         'Damage_low': 20
 #     }
 
+# def test_is_dead():
+#     gladiator = {'Health': 0, 'Damage_high': 30, 'Rage': 52, 'Damage_low': 20}
+#     assert core.is_dead(gladiator) == True
 
-def test_is_dead():
-    gladiator = 0
-    assert core.is_dead(gladiator) == True
+# def test_attack():
