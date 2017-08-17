618b6a35fc279d869ae00ca2b810e00b047318ad
Jose Vargas <jvargas@basecampcodingacademy.org>
2017-08-17 08:55:30 -0500

    cleaner code and working

--- shell.py
+++ shell.py
@@ -27,17 +27,45 @@ def main():
     damage_low = 10
     damage_high = 25
 
-    print('\nLETS FIIIIIIGGGHHHTTT!!!\n\n')
+    print('\nLET\'S FIIIIIIGGGHHHTTT!!!\n\n')
     time.sleep(1)
 
-    gladiator_1 = core.new_gladiator(health, rage, damage_low, damage_high)
-    gladiator_2 = core.new_gladiator(health, rage, damage_low, damage_high)
+    gladiator_1 = core.new_gladiator('Chicharito', health, rage, damage_low,
+                                     damage_high)
+    gladiator_2 = core.new_gladiator2('Messy', health, rage, damage_low,
+                                      damage_high)
+    while True:
+        print('----------------------------------------------------------')
+        print('|{}: HP {}, Rage {}, || {}: Hp {}, Rage {}| '.format(
+            gladiator_1['Name'], gladiator_1['Health'], gladiator_1['Rage'],
+            gladiator_2['Name'], gladiator_2['Health'], gladiator_2['Rage']))
 
-    print('Gladiator_1 HP {}, Rage {}, || Gladiator_2 Hp {}, Rage {} '.format(
-        gladiator_1['Health'], gladiator_1['Rage'], gladiator_2['Health'],
-        gladiator_2['Rage']))
+        print('----------------------------------------------------------')
 
-    decision = slow_type('\nWhat do you want to do? \nAttack\nHeal\n: ')
+        print('\nIt is ' + gladiator_1['Name'] + '\'s turn!')
+        time.sleep(1)
+
+        decision_1 = slow_type(
+            '\n\nWhat do you want to do? \n\n\n1.Attack\n\n2.Heal\n\n: ')
+        if decision_1 == '1':
+            core.attack(gladiator_1, gladiator_2)
+            # print(status)
+            time.sleep(1)
+        elif decision_1 == '2':
+            core.heal(gladiator_1)
+            # print(status)
+
+        print('\n\nIt\'s Messy\'s Turn!\n')
+        time.sleep(1)
+        decision_2 = slow_type(
+            '\n\nWhat do you want to do? \n\n\n1.Attack\n\n2.Heal\n\n: ')
+
+        if decision_2 == '1':
+            core.attack(gladiator_2, gladiator_1)
+            time.sleep(1)
+        elif decision_2 == '2':
+            core.heal(gladiator_2)
+            # print(status)
 
 
 if __name__ == '__main__':
