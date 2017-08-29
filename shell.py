ad41ad5d1ad0e2aed2db3a4f8ce6805285e6632f
Jose Vargas <jvargas@basecampcodingacademy.org>
2017-08-29 16:23:24 -0500

    works good

--- shell.py
+++ shell.py
@@ -13,7 +13,7 @@ def main():
         assigned pace.
         """
 
-        typing_speed = 10
+        typing_speed = 22
 
         for l in t:
             sys.stdout.write(l)
@@ -22,50 +22,120 @@ def main():
 
         return input()
 
-    health = 100
-    rage = 0
-    damage_low = 10
-    damage_high = 25
+    print('Get..Ready...To...Fight...'.center(130))
+    time.sleep(1)
+    slow_type('\nPress "ENTER" To Begin!\n')
 
-    print('\nLET\'S FIIIIIIGGGHHHTTT!!!\n\n')
+    jose = core.Gladiator('JoVarg', 100, 25, 15, 30)
+    liza = core.Gladiator('LizOstro', 100, 25, 15, 30)
+
+    print('☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠'.center(130))
+    print(jose)
+    time.sleep(.5)
+    print('☠☠☠☠☠☠☠VS☠☠☠☠☠☠☠'.center(130))
+    time.sleep(1)
+    print(liza)
+    print('☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠'.center(130))
     time.sleep(1)
 
-    gladiator_1 = core.new_gladiator('Chicharito', health, rage, damage_low,
-                                     damage_high)
-    gladiator_2 = core.new_gladiator2('Messy', health, rage, damage_low,
-                                      damage_high)
     while True:
-        print('----------------------------------------------------------')
-        print('|{}: HP {}, Rage {}, || {}: Hp {}, Rage {}| '.format(
-            gladiator_1['Name'], gladiator_1['Health'], gladiator_1['Rage'],
-            gladiator_2['Name'], gladiator_2['Health'], gladiator_2['Rage']))
+        print('\n\n\n\n')
+        print(jose)
+        print('--------------------------------------------------------------'.
+              center(135))
+        print(liza)
 
-        print('----------------------------------------------------------')
+        slap = core.Move('Slap', randint(0, 25), 15, 20)
+        kick = core.Move('Kick', randint(10, 30), 36, 23)
+        bite = core.Move('Bite', randint(15, 40), 27, 16)
+        slam = core.Move('Slam', randint(30, 60), 43, 34)
+        shoot = core.Move('Shoot', randint(70, 90), 43, 40)
+        conserve = core.Move('Conserve', 0, 0, 25)
+        heal = core.Move('Heal', 0, 5, 30)
+        liza_sound = core.Move('1.Liza AHH? Sound', 100, 60, 45)
+        jose_scream = core.Move('2.Jose AEHH! Scream', 80, 60, 45)
 
-        print('\nIt is ' + gladiator_1['Name'] + '\'s turn!')
+        if jose.is_dead():
+            print('JoVarg! You Are Dead!'.center(130))
+            exit()
+
+        print('\n\n')
+        print('-> JoVarg <- -Make Your Move!-'.center(130))
+        print(
+            '-> {}\n\n-> {}\n\n-> {}\n\n-> {}\n\n-> {}\n\n-> {}\n\n-> {}\n\n-> {}\n\n-> {}\n\n'.
+            format(slap, kick, bite, slam, shoot, conserve, heal, liza_sound,
+                   jose_scream))
         time.sleep(1)
 
-        decision_1 = slow_type(
-            '\n\nWhat do you want to do? \n\n\n1.Attack\n\n2.Heal\n\n: ')
-        if decision_1 == '1':
-            core.attack(gladiator_1, gladiator_2)
-            # print(status)
-            time.sleep(1)
-        elif decision_1 == '2':
-            core.heal(gladiator_1)
-            # print(status)
+        choice = slow_type('------->>>>> '.lower())
 
-        print('\n\nIt\'s Messy\'s Turn!\n')
+        if choice == 'slap':
+            message = jose.attack(liza, slap)
+
+        elif choice == 'kick':
+            message = jose.attack(liza, kick)
+
+        elif choice == 'bite':
+            message = jose.attack(liza, bite)
+
+        elif choice == 'slam':
+            message == jose.attack(liza, slam)
+
+        elif choice == 'shoot':
+            message = jose.attack(liza, shoot)
+
+        elif choice == 'heal':
+            message = jose.heal(heal)
+
+        elif choice == 'conserve':
+            message = jose.conservative(conserve)
+
+        else:
+            print('Invalid Choice'.center(130))
+
+        print(message)
         time.sleep(1)
-        decision_2 = slow_type(
-            '\n\nWhat do you want to do? \n\n\n1.Attack\n\n2.Heal\n\n: ')
 
-        if decision_2 == '1':
-            core.attack(gladiator_2, gladiator_1)
+        if liza.is_dead():
+            print('LizOstro! You Are Dead!'.center(130))
+            exit()
+        print('\n\n')
+        print('-> LizOstro <- -Make Your Move!-'.center(130))
+        print(
+            '-> {}\n\n-> {}\n\n-> {}\n\n-> {}\n\n-> {}\n\n-> {}\n\n-> {}\n\n-> {}\n\n-> {}\n\n'.
+            format(slap, kick, bite, slam, shoot, conserve, heal, liza_sound,
+                   jose_scream))
+        time.sleep(1)
+
+        choice = slow_type('------->>>>> '.lower())
+
+        if choice == 'slap':
+            message = liza.attack(jose, slap)
+
+        elif choice == 'kick':
+            message = liza.attack(jose, kick)
+
+        elif choice == 'bite':
+            message = liza.attack(jose, bite)
+
+        elif choice == 'slam':
+            message == liza.attack(jose, slam)
+
+        elif choice == 'shoot':
+            message = liza.attack(jose, shoot)
+
+        elif choice == 'heal':
+            message = liza.heal(heal)
+
+        elif choice == 'conserve':
+            message = liza.conservative(conserve)
+
             time.sleep(1)
-        elif decision_2 == '2':
-            core.heal(gladiator_2)
-            # print(status)
+        else:
+            print('Invalid Choice!'.center(130))
+
+        print(message)
+        time.sleep(1)
 
 
 if __name__ == '__main__':
