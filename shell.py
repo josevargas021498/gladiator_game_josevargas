161cc33edfa67af86fde27cead7cc870884c1ae1
Jose Vargas <jvargas@basecampcodingacademy.org>
2017-08-30 13:14:24 -0500

    cleaner effective code, works as should

--- shell.py
+++ shell.py
@@ -22,20 +22,20 @@ def main():
 
         return input()
 
-    print('Get..Ready...To...Fight...'.center(130))
+    print('Get..Ready...To...Fight...'.center(135))
     time.sleep(1)
     slow_type('\nPress "ENTER" To Begin!\n')
 
-    jose = core.Gladiator('JoVarg', 100, 25, 15, 30)
-    liza = core.Gladiator('LizOstro', 100, 25, 15, 30)
+    jose = core.Gladiator('JoVarg', 200, 30, 15, 30)
+    liza = core.Gladiator('LizOstro', 200, 30, 15, 30)
 
-    print('☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠'.center(130))
+    print('☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠'.center(135))
     print(jose)
     time.sleep(.5)
-    print('☠☠☠☠☠☠☠VS☠☠☠☠☠☠☠'.center(130))
+    print('☠☠☠☠☠☠☠VS☠☠☠☠☠☠☠'.center(135))
     time.sleep(1)
     print(liza)
-    print('☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠'.center(130))
+    print('☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠'.center(135))
     time.sleep(1)
 
     while True:
@@ -45,24 +45,26 @@ def main():
               center(135))
         print(liza)
 
-        slap = core.Move('Slap', randint(0, 25), 15, 20)
-        kick = core.Move('Kick', randint(10, 30), 36, 23)
-        bite = core.Move('Bite', randint(15, 40), 27, 16)
-        slam = core.Move('Slam', randint(30, 60), 43, 34)
-        shoot = core.Move('Shoot', randint(70, 90), 43, 40)
-        conserve = core.Move('Conserve', 0, 0, 25)
-        heal = core.Move('Heal', 0, 5, 30)
-        liza_sound = core.Move('1.Liza AHH? Sound', 100, 60, 45)
-        jose_scream = core.Move('2.Jose AEHH! Scream', 80, 60, 45)
+        slap = core.Move('SLAP', randint(0, 25), 15, 20)
+        kick = core.Move('KICK', randint(10, 30), 36, 23)
+        bite = core.Move('BITE', randint(15, 40), 27, 16)
+        slam = core.Move('SLAM', randint(30, 60), 43, 24)
+        shoot = core.Move('SHOOT', randint(70, 90), 43, 29)
+        conserve = core.Move('CONSERVE', 0, 0, 25)
+        heal = core.Move('HEAL', 0, 5, 30)
+        liza_sound = core.Move('1.LIZA "AAHHHHH??" SOUND', 100, 60, 45)
+        jose_scream = core.Move('2.JOSE "AAEEEEE!!!" SCREAM', 80, 60, 45)
 
         if jose.is_dead():
-            print('JoVarg! You Are Dead!'.center(130))
+            print('⚰⚰⚰⚰⚰⚰⚰⚰⚰⚰⚰⚰⚰⚰⚰⚰⚰'.center(135))
+            print('JoVarg! You Are Dead!'.center(135))
+            print('⚰⚰⚰⚰⚰⚰⚰⚰⚰⚰⚰⚰⚰⚰⚰'.center(135))
             exit()
 
         print('\n\n')
-        print('-> JoVarg <- -Make Your Move!-'.center(130))
+        print('-> JoVarg! <- -Make Your Move!-'.center(135))
         print(
-            '-> {}\n\n-> {}\n\n-> {}\n\n-> {}\n\n-> {}\n\n-> {}\n\n-> {}\n\n-> {}\n\n-> {}\n\n'.
+            '->👋 {}\n\n->☯ {}\n\n->😁 {}\n\n->💥 {}\n\n->🔫 {}\n\n->♻ {}\n\n->♥ {}\n\n->☢☣💀 {} 💀💀\n\n->♬♬ {}\n\n'.
             format(slap, kick, bite, slam, shoot, conserve, heal, liza_sound,
                    jose_scream))
         time.sleep(1)
@@ -91,18 +93,31 @@ def main():
             message = jose.conservative(conserve)
 
         else:
-            print('Invalid Choice'.center(130))
+            print('Invalid Choice'.center(135))
 
         print(message)
         time.sleep(1)
 
         if liza.is_dead():
-            print('LizOstro! You Are Dead!'.center(130))
+            print('⚰⚰⚰⚰⚰⚰⚰⚰⚰⚰⚰⚰⚰⚰⚰⚰⚰'.center(135))
+            print('LizOstro! You Are Dead!'.center(135))
+            print('⚰⚰⚰⚰⚰⚰⚰⚰⚰⚰⚰⚰⚰⚰⚰⚰⚰'.center(135))
             exit()
+
+        slap = core.Move('SLAP->->', randint(5, 25), 15, 20)
+        kick = core.Move('KICK->->', randint(18, 39), 36, 23)
+        bite = core.Move('BITE->->', randint(27, 51), 27, 16)
+        slam = core.Move('SLAM->->', randint(45, 70), 43, 34)
+        shoot = core.Move('SHOOT->->', randint(70, 90), 43, 40)
+        conserve = core.Move('CONSERVE->->', 0, 0, 25)
+        heal = core.Move('HEAL->->', 0, 5, 30)
+        liza_sound = core.Move('\n1.LIZA "AAHHHHH??" SOUND', 100, 60, 45)
+        jose_scream = core.Move('\n2.JOSE "AAEEEE!!!" SCREAM', 90, 60, 45)
+
         print('\n\n')
-        print('-> LizOstro <- -Make Your Move!-'.center(130))
+        print('-> LizOstro! <- -Make Your Move!-'.center(135))
         print(
-            '-> {}\n\n-> {}\n\n-> {}\n\n-> {}\n\n-> {}\n\n-> {}\n\n-> {}\n\n-> {}\n\n-> {}\n\n'.
+            '->👋 {}\n\n->☯ {}\n\n->😁 {}\n\n->💥 {}\n\n->🔫 {}\n\n->♻ {}\n\n->♥ {}\n\n->☢☣💀 {} 💀💀\n\n->♬♬ {}\n\n'.
             format(slap, kick, bite, slam, shoot, conserve, heal, liza_sound,
                    jose_scream))
         time.sleep(1)
@@ -132,7 +147,7 @@ def main():
 
             time.sleep(1)
         else:
-            print('Invalid Choice!'.center(130))
+            print('Invalid Choice!'.center(135))
 
         print(message)
         time.sleep(1)
